dec53a4a151275b55ca03a2432986c1d96c6cbc1
Vishrut Aggarwal <vishrut.agg@gmail.com>
2022-01-22 14:23:00 +0530

    completed desktop version

--- portfolio/src/components/ExpPage.js
+++ portfolio/src/components/ExpPage.js
@@ -3,8 +3,32 @@ import React, { Component } from 'react';
 class ExpPage extends Component {
   render() {
     return (
-        <div className = "page">
-            
+        <div className = "page bg-light" id = "experience-page">
+            <section id = "exp-heading">
+              <h1>Experience</h1>
+            </section>
+            <section id = "exp-content">
+              <div className = "exp">
+                <div className = "exp-workplace">
+                  LOREM IPSUM
+                </div>
+                <div className = "exp-details">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam fugiat
+                nulla perspiciatis ipsam voluptatem temporibus provident reiciendis.
+                Magnam, maiores autem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, natus.
+                </div>
+              </div>
+              <div className = "exp">
+                <div className = "exp-workplace">
+                  LOREM IPSUM
+                </div>
+                <div className = "exp-details">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam fugiat
+                nulla perspiciatis ipsam voluptatem temporibus provident reiciendis.
+                Magnam, maiores autem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, natus.
+                </div>
+              </div>
+            </section>
         </div>
     )
   }
